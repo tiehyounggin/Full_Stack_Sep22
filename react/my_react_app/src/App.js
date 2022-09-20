84ed4de0a89d098ba0650e5389ce1dd97ba29851
yg <blueyounggin@hotmail.com>
2022-09-20 18:04:55 +0800

    add

---
+++ react/my_react_app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello there. Welcome to my first react - YG
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React NOW or LATER... or NEVER
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
