081565c5d02216ed620ea3c0a0da79f262c0c584
yg <tiehyounggin@gmail.com>
2022-10-06 15:47:37 +0800

    updatetto

---
+++ java/aJavaProject/src/Homework_0610/Main.java
@@ -0,0 +1,61 @@
+package Homework_0610;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<User> myArrList = new ArrayList<>();
+        String pathName = Paths.get("").toAbsolutePath().toString() + "\\src\\Homework_0610\\";
+        String fileName = "Sample File.txt";
+
+        for(int i=1; i<=10; i++){
+            myArrList.add(new User("name " + i, "address " + i));
+        }
+
+        File aFile = new File(pathName + fileName);
+
+        if(aFile.exists()){
+            System.out.println("File found");
+        }else{
+            System.out.println("File is nowhere to be found");
+            try{
+                aFile.createNewFile();
+                System.out.println("File is created successfully!");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        for (User user: myArrList ) {
+            writeToFile(aFile, user);
+        }
+
+    }
+
+    public static void writeToFile(File filename, User user){
+
+        String myString = "";
+
+        try{
+            FileOutputStream fileOutputStream = new FileOutputStream(filename, true);
+
+            LocalDateTime localDateTime = LocalDateTime.now();
+            String formattedLocalDateTime = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).format(localDateTime);
+
+            myString = "[" + formattedLocalDateTime + "] " + user.getName() + ", " + user.getAddress() + "\r\n";
+
+            fileOutputStream.write(myString.getBytes());
+            fileOutputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java/aJavaProject/src/Homework_0610/User.java
@@ -0,0 +1,31 @@
+package Homework_0610;
+
+public class User {
+    private String name = "";
+    private String address = "";
+
+    public User(){
+
+    }
+
+    public User(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
